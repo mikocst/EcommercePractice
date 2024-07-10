3a0a38cb8676969036b2364a00b14df01dfb8b4c
Miko Castillo <mikocastillo7@gmail.com>
2024-07-09 19:11:34 -0500

    modal implementation

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+//Variable Initialization
+    //Item Count Variables
 var minus = document.getElementById("minus");
 var plus = document.getElementById("plus");
 var itemCount = document.getElementById("item-count");
 var count = 0;
 
+    //Modal Variables
+var dialog = document.querySelector("dialog");
+var showImage1 = document.getElementById("modal1")
+var showImage2 = document.getElementById("modal2")
+var showImage3 = document.getElementById("modal3")
+var showImage4 = document.getElementById("modal4")
+
+
+
+//Counter functions for item stock
 function plusCount () {
     count += 1;
     itemCount.textContent = count;
@@ -12,3 +24,22 @@ function minusCount() {
     count -= 1;
     itemCount.textContent = count;
 }
+
+//Modal Listeners for showing image previews
+    //Opening Modals
+showImage1.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+showImage2.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+showImage3.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+showImage4.addEventListener("click", () => {
+    dialog.showModal();
+})
+
