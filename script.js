4f42c5ed1d3e70e91ae8d7ee0352332f6f4f14ab
Miko Castillo <mikocastillo7@gmail.com>
2024-07-10 00:58:42 -0500

    modal styling and closing state

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-//Variable Initialization
-    //Item Count Variables
 var minus = document.getElementById("minus");
 var plus = document.getElementById("plus");
 var itemCount = document.getElementById("item-count");
 var count = 0;
 
-    //Modal Variables
-var dialog = document.querySelector("dialog");
-var showImage1 = document.getElementById("modal1")
-var showImage2 = document.getElementById("modal2")
-var showImage3 = document.getElementById("modal3")
-var showImage4 = document.getElementById("modal4")
-
-
+var prodDialog = document.getElementById("product-dialog");
+var showImage1 = document.getElementById("modal1");
+var showImage2 = document.getElementById("modal2");
+var showImage3 = document.getElementById("modal3");
+var showImage4 = document.getElementById("modal4");
+var closeIcon = document.getElementById("close-icon");
 
 //Counter functions for item stock
 function plusCount () {
@@ -26,20 +22,28 @@ function minusCount() {
 }
 
 //Modal Listeners for showing image previews
-    //Opening Modals
 showImage1.addEventListener("click", () => {
-    dialog.showModal();
-})
+    console.log('1 is clicked');
+    prodDialog.showModal();
+});
 
 showImage2.addEventListener("click", () => {
-    dialog.showModal();
-})
+    console.log('2 is clicked');
+    prodDialog.showModal();
+});
 
 showImage3.addEventListener("click", () => {
-    dialog.showModal();
-})
+    console.log('3 is clicked');
+    prodDialog.showModal();
+});
 
 showImage4.addEventListener("click", () => {
-    dialog.showModal();
-})
+    console.log('4 is clicked');
+    prodDialog.showModal();
+});
 
+//Closing Modal
+closeIcon.addEventListener("click", () => {
+  console.log('close clicked');
+  prodDialog.close();
+});
